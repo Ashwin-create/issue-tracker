3d49907b4d87edcf13c7faaadc47167206d33ac1
ashwinmalekar <79403198+Ashwin-create@users.noreply.github.com>
2024-04-08 01:27:02 +0100

    Homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <>
+    <div className="text-3xl">Issue Tracker</div>
+    <div className="text-xl italic">Create new issues, assign and update. Get ahead on your pending workflow</div>
+    </>
   )
 }
